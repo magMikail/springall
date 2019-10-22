d1dcce0604c1a2372748aa72d0bc899530fe5685
magmik <mag.mikail@gmail.com>
2019-10-22 23:54:49 +0200

    jpa

---
+++ HELP.md
@@ -0,0 +1,35 @@
+# This is an <h1> tag
+*This text will be italic*
+_This will also be italic_
+
+1. Item 1
+   1. Item 3a
+> We're living the future so
+
+
+I think you should use an
+`<addr>` element here instead.
+
+```javascript
+function fancyAlert(arg) {
+  if(arg) {
+    $.facebox({div:'#foo'})
+  }
+}
+```
+    function fancyAlert(arg) {
+      if(arg) {
+        $.facebox({div:'#foo'})
+      }
+    }
+- [x] @mentions, #refs, [links](), **formatting**, and <del>tags</del> supported
+- [x] this is a complete item
+- [ ] this is an incomplete item
+
+First Header | Second Header
+------------ | -------------
+Content from cell 1 | Content from cell 2
+Content in the first column | Content in the second column
+
+~~this~~
+
---
+++ spring-data-jpa/src/main/java/com/magmik/example/entity/Deal.java
@@ -0,0 +1,43 @@
+package com.magmik.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by magMikail on 10/15/2019.
+ **/
+@Entity
+public class Deal {
+    @Id
+    @GeneratedValue
+    Long id;
+    Date startDate;
+    String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
