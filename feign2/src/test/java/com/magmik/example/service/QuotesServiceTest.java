0aef31ba9f6957caea029891def92d0c467a6048
magMikail <mag.mikail@gmail.com>
2020-01-25 22:23:07 +0100

    test dependencies

---
+++ feign2/src/test/java/com/magmik/example/service/QuotesServiceTest.java
@@ -0,0 +1,18 @@
+package com.magmik.example.service;
+
+import org.junit.jupiter.api.Test;
+
+public class QuotesServiceTest {
+    @Test
+    public void getById() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void getApps() throws Exception {
+    }
+
+}
