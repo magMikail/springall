3e8e9d51ccce6f018a525c9cd64905a9bc59f60d
magMikail <mag.mikail@gmail.com>
2020-01-25 23:32:40 +0100

    start feign mock

--- feign2/src/test/java/com/magmik/example/service/QuotesServiceTest.java
+++ feign2/src/test/java/com/magmik/example/service/QuotesServiceTest.java
@@ -1,18 +1,36 @@
 package com.magmik.example.service;
 
+import feign.Feign;
+import feign.gson.GsonDecoder;
+import feign.mock.HttpMethod;
+import feign.mock.MockClient;
+import feign.mock.MockTarget;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class QuotesServiceTest {
-    @Test
-    public void getById() throws Exception {
+
+
+    private DataService dataService;
+    private MockClient mockClient;
+
+    @Before
+    public void setup() throws IOException {
+        mockClient = new MockClient()
+                .noContent(HttpMethod.PATCH, "/repos/velo/feign-mock/contributors");
+
+        dataService = Feign.builder()
+                .decoder(new GsonDecoder())
+                .client(mockClient)
+                .target(new MockTarget<>(DataService.class));
     }
 
     @Test
-    public void getAll() throws Exception {
+    public void missHttpMethod() {
+//        List<Contributor> result = dataService.patchContributors("velo", "feign-mock");
+//        assertThat(result, nullValue());
+//        mockClient.verifyOne(HttpMethod.PATCH, "/repos/velo/feign-mock/contributors");
     }
-
-    @Test
-    public void getApps() throws Exception {
-    }
-
 }
